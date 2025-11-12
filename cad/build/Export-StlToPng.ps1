f15ad66d59c48bfe9c7335e4335590b99f14848a
Pedro Paulo Vezza Campos <pedro@vezza.com.br>
2025-11-12 11:10:48 -0800

    Remove crosshairs from render view options

--- cad/build/Export-StlToPng.ps1
+++ cad/build/Export-StlToPng.ps1
@@ -127,7 +127,7 @@ function Invoke-StlRender {
             "--imgsize=$ImageSize,$ImageSize"
             "--camera=$($angle.Value)"
             "--colorscheme=$ColorScheme"
-            '--view=axes,scales,edges,crosshairs'
+            '--view=axes,scales,edges'
             '--viewall'
             '--projection=o'
             '--render'
