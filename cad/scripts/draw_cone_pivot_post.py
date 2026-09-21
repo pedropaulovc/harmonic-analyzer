482c4b0200fbdfc72a515f6e1f53be1cfb8126a3
Pedro Paulo Vezza Campos <pedro@vezza.com.br>
2026-09-21 13:04:37 -0700

    drawings: fit pivot finish corridor

--- cad/scripts/draw_cone_pivot_post.py
+++ cad/scripts/draw_cone_pivot_post.py
@@ -757,7 +757,7 @@ async def build(adapter: Any) -> dict[str, str]:
         adapter,
         front,
         edge_entity=_bore_rim_edge(front, diameter_mm=CRANK_BORE_DIA),
-        symbol_xy=(0.055, 0.153),
+        symbol_xy=(0.055, 0.145),
         leader_attach_xy=model_point_in_view(
             adapter,
             front,
