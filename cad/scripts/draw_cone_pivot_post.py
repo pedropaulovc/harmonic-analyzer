ee0a5151c71d7008e8cb217f4e5921cdde71399d
Pedro Paulo Vezza Campos <pedro@vezza.com.br>
2026-09-21 11:27:31 -0700

    drawings: reroute pivot finish leader

--- cad/scripts/draw_cone_pivot_post.py
+++ cad/scripts/draw_cone_pivot_post.py
@@ -751,7 +751,7 @@ async def build(adapter: Any) -> dict[str, str]:
         adapter,
         front,
         edge_entity=_bore_rim_edge(front, diameter_mm=CRANK_BORE_DIA),
-        symbol_xy=(0.185, 0.122),
+        symbol_xy=(0.185, 0.116),
         leader_attach_xy=model_point_in_view(
             adapter,
             front,
